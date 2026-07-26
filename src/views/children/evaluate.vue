566a400922790839df1e9eba205d00788d894d67
MohammadFalahFront <mohammadfalahfront@gmail.com>
2026-07-27 02:19:32 +0300

    fix: validate parameters in go_evaluate method and correct child_id reference

--- src/views/children/evaluate.vue
+++ src/views/children/evaluate.vue
@@ -180,8 +180,15 @@
 
 
       },
-      go_evaluate(id,type,child_id,data){a
+
+      go_evaluate(id,type,child_id,data){
         
+        if(!data.pivot.child_id || !data.side_profile_id || !id) {
+          
+          this.$toast.add({ severity: 'error', summary: this.$t("error"), detail: `${this.$t("mission_error")}`, life: 3000 });
+          return;
+        }
+
         if(type==2){
           this.$router.push({ name: 'milestone-resulte', params:{'id':child_id,'evla_id':id}});
         } 
@@ -194,7 +201,7 @@
         if(type==5){
           this.$router.push({ name: 'carolina-resulte', params:{'id':id}});
         }else{
-          this.$router.push({ name: 'showChildResult', params:{'child_id':data.pivot.child_idz,sideProfile_id:data.side_profile_id,evaluation_id:id}});
+          this.$router.push({ name: 'showChildResult', params:{'child_id':data.pivot.child_id,sideProfile_id:data.side_profile_id,evaluation_id:id}});
         }
       },
 
